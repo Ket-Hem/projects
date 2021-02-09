f0fa62e06e174479be170d481d2312f390fc2853
Ket-Hem <dimasayjk@gmail.com>
2021-02-09 13:31:41 +0200

    * repeatArrayValues - 1.0 * getUniqueValue - 1.0 * groupByTag -1.0

--- Test.php
+++ Test.php
@@ -7,7 +7,6 @@
         h1{
             color: #363636;
             text-align: center;
-            font-family: Calibri;
             font-size: 250%;
         }
     </style>
@@ -15,43 +14,12 @@
 </head>
 <body>
 <?php
-//echo phpinfo();
-echo "<h1>".date('jS F Y  H:i:s')."</h1></br>";
+date_default_timezone_set ( 'Europe/Kiev');
+echo "<h1>".basename(__FILE__).date(" jS F Y  H:i:s")."</h1><hr>";
+/****************************************************************************/
 
-/**
- * The $minute variable contains a number from 0 to 59 (i.e. 10 or 25 or 60 etc).
- * Determine in which quarter of an hour the number falls.
- * Return one of the values: "first", "second", "third" and "fourth".
- * Throw InvalidArgumentException if $minute is negative of greater then 60.
- * @see https://www.php.net/manual/en/class.invalidargumentexception.php
- *
- * @param  int  $minute
- * @return string
- * @throws InvalidArgumentException
- */
-function getMinuteQuarter(int $minute=0)
-{
-    if ($minute < 0 || $minute >= 60) {
-        throw new InvalidArgumentException("The number: $minute - doesn\`t fit in minute range. ");
-    } elseif ($minute>= 0 && $minute < 15) {
-        $answer = "first";
-    } elseif ($minute >= 15 && $minute < 30) {
-        $answer = "second";
-    } elseif ($minute >= 30 && $minute < 45) {
-        $answer = "third";
-    } else {
-        $answer = "fourth";
-    }
-    return $answer;
-}
 
-echo getMinuteQuarter(58);
 
-function tripleInteger($int)
-{
-    if(!is_int($int))
-        throw new InvalidArgumentException('tripleInteger function only accepts integers. Input was: '.$int);
-    return $int * 3;
-}
+/*****************************************************************************/
 ?>
 </body>
