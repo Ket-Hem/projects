ba6c9c24c05988824555345e2ce98f11338cd7d9
Ket-Hem <dimasayjk@gmail.com>
2021-03-08 14:19:03 +0200

    collect.php

---
+++ The vault for function/collect.php
@@ -0,0 +1,20 @@
+<?php
+function collect($arr, $callback)
+{
+    foreach ($arr as $value) {
+        // перевіряє чи значення НЕ є парним
+        if ($value % 2 !== 0) {
+
+            yield $callback($value);
+        }
+    }
+}
+// масив для перебору
+$arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+//змінна в якій накопичуються значення що пройшли фільтрацію
+//функції collect переданй масив $arr і анонімна функція
+$collect = collect($arr, function ($e) {
+    return $e * $e;
+});
+//виводить всі значення масиву
+foreach ($collect as $val) echo "$val<br/>";
